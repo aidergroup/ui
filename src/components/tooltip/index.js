82c28207bae0e8c706241627c75cb0249fe354d9
Albin Martinsson <albinmartinsson91@gmail.com>
2021-03-22 09:52:10 +0100

    tooltip component added :thumbsup:

---
+++ src/components/tooltip/index.js
@@ -0,0 +1,21 @@
+import {
+  Root,
+  Trigger as RadixTrigger,
+  Content as RadixContent,
+  Arrow as RadixArrow,
+} from '@radix-ui/react-dropdown-menu'
+import tw, { styled } from 'twin.macro'
+
+const Trigger = styled(RadixTrigger)`
+  ${tw`focus:outline-none`}
+`
+
+const Content = styled(RadixContent)`
+  ${tw`px-2 py-1 items-center flex bg-gray-400 rounded-md`}
+`
+
+const Arrow = styled(RadixArrow)`
+  ${tw`fill-current text-gray-400`}
+`
+
+export { Root, Trigger, Content, Arrow }
