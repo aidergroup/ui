c53c4d1a33c0a6f90ab9806dce8fed49cf2c192f
Albin Martinsson <albinmartinsson91@gmail.com>
2021-06-14 14:15:57 +0200

    update filter button active styles

--- src/components/filter-button/index.js
+++ src/components/filter-button/index.js
@@ -8,16 +8,18 @@ const FilterButton = React.forwardRef((props, ref) => {
 
   return (
     <button
-      className={`flex items-center space-x-2 shadow-sm rounded-lg focus:outline-none transition py-1.5 px-4 focus:ring-2 ring-blue-400 bg-white border border-gray-400 duration-300 disabled:bg-gray-300 disabled:shadow-none disabled:cursor-not-allowed ${
-        value && 'ring-2'
-      }`}
+      className="flex items-center space-x-2 rounded-lg focus:outline-none transition py-2 px-4 focus:ring-2 ring-blue-400 bg-white border border-gray-400 duration-300 disabled:bg-gray-300 disabled:shadow-none disabled:cursor-not-allowed"
       type="button"
       style={{ width: '160px' }}
       ref={ref}
       {...rest}
     >
       <span className="font-medium text-left text-gray-800">{title}</span>
-      <span className="font-medium text-left text-black truncate flex-1">
+      <span
+        className={`${
+          value ? 'text-black' : 'text-gray-700'
+        } font-medium text-left text-black truncate flex-1`}
+      >
         {value || 'Visa alla'}
       </span>
       <Icon name="chevron-down" className="h-3 w-3 fill-current" />
