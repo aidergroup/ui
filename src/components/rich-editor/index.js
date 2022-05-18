0a5bfb35bfe758ebf9f2eee1fd1ccb7432492a4b
Corentin Laithier <corentin@papaja.se>
2022-05-18 14:53:09 +0200

    FEAT: Improves the Rich Text Editor

--- src/components/rich-editor/index.js
+++ src/components/rich-editor/index.js
@@ -1,71 +1,99 @@
 import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 import { Editor, RichUtils } from 'draft-js'
+import 'draft-js/dist/Draft.css'
 
 import Icon from '../icon'
 import IconButton from '../icon-button'
+import Button from '../button'
 
 const RichEditor = ({ editorState, onChange, error, actions, disabled }) => {
-	const editor = useRef(null)
+  const editor = useRef(null)
 
-	function focusEditor() {
-		editor.current.focus()
-	}
+  function focusEditor() {
+    editor.current.focus()
+  }
 
-	const applyStyle = style => {
-		onChange(RichUtils.toggleInlineStyle(editorState, style))
-	}
+  const applyStyle = style => {
+    onChange(RichUtils.toggleInlineStyle(editorState, style))
+  }
 
-	return (
-		<div
-			className={`border border-gray-300 rounded-lg w-full overflow-hidden bg-white ${
-				disabled && 'cursor-not-allowed'
-			}`}
-		>
-			<div
-				className="py-4 px-4 overflow-scroll hover:cursor-text"
-				onClick={focusEditor}
-			>
-				<Editor
-					ref={editor}
-					readOnly={disabled}
-					editorState={editorState}
-					onChange={onChange}
-				/>
-				{!!error && (
-					<div className="px-3 py-1 bg-red mt-2 text-white inline-block rounded text-sm font-medium">
-						{error}
-					</div>
-				)}
-			</div>
-			<div className="px-3 flex py-2 justify-between items-center border-t border-gray-300">
-				<div className="flex">
-					<IconButton
-						type="button"
-						disabled={disabled}
-						tertiary
-						icon={<Icon name="bold" className="h-3 w-3 text-black" />}
-						onClick={() => applyStyle('BOLD')}
-					/>
-					<IconButton
-						type="button"
-						disabled={disabled}
-						tertiary
-						icon={<Icon name="italic" className="h-3 w-3 text-black" />}
-						onClick={() => applyStyle('ITALIC')}
-					/>
-					<IconButton
-						type="button"
-						disabled={disabled}
-						tertiary
-						icon={<Icon name="underline" className="h-3 w-3 text-black" />}
-						onClick={() => applyStyle('UNDERLINE')}
-					/>
-				</div>
-				<div>{!!actions && actions}</div>
-			</div>
-		</div>
-	)
+  const toggleBlockType = blockType => {
+    onChange(RichUtils.toggleBlockType(editorState, blockType))
+  }
+
+  const getBlockStyle = block => {
+    switch (block.getType()) {
+      case 'blockquote':
+        return 'RichEditor-blockquote'
+      default:
+        return null
+    }
+  }
+
+  return (
+    <div
+      className={`border border-gray-300 rounded-lg w-full overflow-hidden bg-white ${
+        disabled && 'cursor-not-allowed'
+      }`}
+    >
+      <div
+        className="px-4 py-4 overflow-scroll hover:cursor-text"
+        onClick={focusEditor}
+      >
+        <Editor
+          ref={editor}
+          readOnly={disabled}
+          editorState={editorState}
+          onChange={onChange}
+          blockStyleFn={getBlockStyle}
+        />
+        {!!error && (
+          <div className="inline-block px-3 py-1 mt-2 text-sm font-medium text-white rounded bg-red">
+            {error}
+          </div>
+        )}
+      </div>
+      <div className="flex items-center justify-between px-3 py-2 border-t border-gray-300">
+        <div className="flex">
+          <IconButton
+            type="button"
+            disabled={disabled}
+            tertiary
+            icon={<Icon name="bold" className="w-3 h-3 text-black" />}
+            onClick={() => applyStyle('BOLD')}
+          />
+          <IconButton
+            type="button"
+            disabled={disabled}
+            tertiary
+            icon={<Icon name="italic" className="w-3 h-3 text-black" />}
+            onClick={() => applyStyle('ITALIC')}
+          />
+          <IconButton
+            type="button"
+            disabled={disabled}
+            tertiary
+            icon={<Icon name="list-bulleted" className="w-3 h-3 text-black" />}
+            onClick={() => toggleBlockType('unordered-list-item')}
+          />
+          <span
+            className="inline-flex items-center px-2 py-2 ml-4 text-xs font-medium tracking-wide uppercase transition duration-300 bg-white rounded-md cursor-pointer hover:bg-gray-300"
+            onClick={() => toggleBlockType('header-one')}
+          >
+            rubrik
+          </span>
+          <span
+            className="inline-flex items-center px-2 py-2 text-xs font-medium tracking-wide uppercase transition duration-300 bg-white rounded-md cursor-pointer hover:bg-gray-300"
+            onClick={() => toggleBlockType('unstyled')}
+          >
+            brödtext
+          </span>
+        </div>
+        <div>{!!actions && actions}</div>
+      </div>
+    </div>
+  )
 }
 
 RichEditor.propTypes = {
